9ec696b85d25b26d8ff6b27ae4cdb4b403a09b44
abed <abod>
2022-09-05 21:29:27 +0300

    0

--- lib/Screens/LoginAndCreeatAccount/LoginScreen.dart
+++ lib/Screens/LoginAndCreeatAccount/LoginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  var asfd   = ' ';
+
   @override
   Widget build(BuildContext context) {
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:scratchfood/Screens/LoginAndCreeatAccount/LoginScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(  MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  var asfd   = ' ';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
