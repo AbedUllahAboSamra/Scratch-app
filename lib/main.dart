4bfc8248d0b6f844147236c1f154a864b8bc3302
abed <abod>
2022-09-05 16:22:28 +0300

    packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:scratchfood/Screens/LoginAndCreeatAccount/LoginScreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:scratchfood/Screens/LoginAndCreeatAccount/LoginScreen.dart';
 
 void main() {
   runApp(const MyApp());
