67063ddea741f090f7efc8fcf27eb056203f3667
AhmedMahdi0 <ahmed.h.b.mahdi@gmail.com>
2022-09-06 13:33:52 +0300

    qwe

--- lib/Screens/MainScreen/HomeScreen/HomeScreen.dart
+++ lib/Screens/MainScreen/HomeScreen/HomeScreen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/cupertino.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text(''),
+    );
   }
 
 }
